2ad6931faa539008d27a634ea115bf6845b9d6dd
lukewaring <lukewaring@gmail.com>
2019-12-30 13:45:08 -0500

    Luke Monday morning work

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // make sure we get the routes right
+
+const recipesDiv = document.getElementById('recipes')
+
+fetch('http://localhost:3000/recipes')
+    .then(function(response) { return response.json() })
+    .then(function(recipes) {
+        recipes.forEach(recipe => {
+            const recipeCard = document.createElement('div')
+            recipeCard.setAttribute('class', 'card')
+            recipeCard.innerHTML = `
+                <img class='photo' src=${recipe.image}>
+                <h4>${recipe.title}
+            `
+            recipeCard.addEventListener('click', function(e) {
+                console.log(e.target)
+            })
+            recipesDiv.appendChild(recipeCard)
+        });
+    })
